27a650b19cc562b076a6ae382f719d78a303906f
Santak Kumar Mishra <1337shan@gmail.com>
2013-09-15 01:47:55 +0530

    Quick Fix
    Corrected IDs on video pages and added highlighting to boxes on photos
    and video listing pages.

--- source/js/main.js
+++ source/js/main.js
@@ -29,7 +29,7 @@ $(".addCollection").on("click",function(event) {
 	$("#systemModal").modal();
 });
 
-$(".collectionView .panel").on("mouseover",function() {
+$(".collectionView .panel, .photoView .panel, .videoView .panel").on("mouseover",function() {
 	$(this).removeClass("panel-default").addClass("panel-primary");
 }).on("mouseout",function() {
 	$(this).removeClass("panel-primary").addClass("panel-default");
--- source/video.php
+++ source/video.php
@@ -44,14 +44,14 @@
 	</div>
 </section>
 
-<section class="container photoView">
+<section class="container videoView">
 	<div class="row">
 		<div class="col-md-3 text-center panel panel-default">
 			<div class="row">
-				<h3 class="photoName">Youtube Video</h3>
+				<h3 class="videoName">Youtube Video</h3>
 				<div class="row">
-					<a class="editPhoto" title="Edit Photo" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
-					<a class="deletePhoto" title="Delete Photo" href="#"><span class="glyphicon glyphicon-trash"></span></a>
+					<a class="editvideo" title="Edit video" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
+					<a class="deletevideo" title="Delete video" href="#"><span class="glyphicon glyphicon-trash"></span></a>
 				</div>
 				<a class="fancybox-media" rel="group" href="http://www.youtube.com/watch?v=2zqy21Z29ps" ><img class="img-thumbnail" src="http://img.youtube.com/vi/2zqy21Z29ps/0.jpg" alt="video" /></a>
 			</div>
@@ -63,17 +63,17 @@
 					<p><strong>Views:<br></strong><em><span class="collectionDate">1260</span></em></p>
 				</div>
 			</div>
-			<p class="photoDesc text-left">
+			<p class="videoDesc text-left">
 			Bacon ipsum dolor sit amet cow swine chicken tri-tip t-bone, jowl short loin short ribs bresaola fra...
 			<button class="btn btn-primary btn-xs">More<span class="glyphicon glyphicon-chevron-right pull-right"></span></button>
 			</p>
 		</div>
 		<div class="col-md-3 text-center panel panel-default">
 			<div class="row">
-				<h3 class="photoName">Youtube Video</h3>
+				<h3 class="videoName">Youtube Video</h3>
 				<div class="row">
-					<a class="editPhoto" title="Edit Photo" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
-					<a class="deletePhoto" title="Delete Photo" href="#"><span class="glyphicon glyphicon-trash"></span></a>
+					<a class="editvideo" title="Edit video" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
+					<a class="deletevideo" title="Delete video" href="#"><span class="glyphicon glyphicon-trash"></span></a>
 				</div>
 				<a class="fancybox-media" rel="group" href="http://www.youtube.com/watch?v=HzS--5tuzHA" ><img class="img-thumbnail" src="http://img.youtube.com/vi/HzS--5tuzHA/0.jpg" alt="video" /></a>
 			</div>
@@ -85,17 +85,17 @@
 					<p><strong>Views:<br></strong><em><span class="collectionDate">1260</span></em></p>
 				</div>
 			</div>
-			<p class="photoDesc text-left">
+			<p class="videoDesc text-left">
 			Bacon ipsum dolor sit amet cow swine chicken tri-tip t-bone, jowl short loin short ribs bresaola fra...
 			<button class="btn btn-primary btn-xs">More<span class="glyphicon glyphicon-chevron-right pull-right"></span></button>
 			</p>
 		</div>
 		<div class="col-md-3 text-center panel panel-default">
 			<div class="row">
-				<h3 class="photoName">Youtube Video</h3>
+				<h3 class="videoName">Youtube Video</h3>
 				<div class="row">
-					<a class="editPhoto" title="Edit Photo" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
-					<a class="deletePhoto" title="Delete Photo" href="#"><span class="glyphicon glyphicon-trash"></span></a>
+					<a class="editvideo" title="Edit video" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
+					<a class="deletevideo" title="Delete video" href="#"><span class="glyphicon glyphicon-trash"></span></a>
 				</div>
 				<a class="fancybox-media" rel="group" href="http://www.youtube.com/watch?v=PjfaZxTTVpU" ><img class="img-thumbnail" src="http://img.youtube.com/vi/PjfaZxTTVpU/0.jpg" alt="video" /></a>
 			</div>
@@ -107,17 +107,17 @@
 					<p><strong>Views:<br></strong><em><span class="collectionDate">1260</span></em></p>
 				</div>
 			</div>
-			<p class="photoDesc text-left">
+			<p class="videoDesc text-left">
 			Bacon ipsum dolor sit amet cow swine chicken tri-tip t-bone, jowl short loin short ribs bresaola fra...
 			<button class="btn btn-primary btn-xs">More<span class="glyphicon glyphicon-chevron-right pull-right"></span></button>
 			</p>
 		</div>
 		<div class="col-md-3 text-center panel panel-default">
 			<div class="row">
-				<h3 class="photoName">Youtube Video</h3>
+				<h3 class="videoName">Youtube Video</h3>
 				<div class="row">
-					<a class="editPhoto" title="Edit Photo" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
-					<a class="deletePhoto" title="Delete Photo" href="#"><span class="glyphicon glyphicon-trash"></span></a>
+					<a class="editvideo" title="Edit video" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
+					<a class="deletevideo" title="Delete video" href="#"><span class="glyphicon glyphicon-trash"></span></a>
 				</div>
 				<a class="fancybox-media" rel="group" href="http://www.youtube.com/watch?v=4axPibsp4Ao" ><img class="img-thumbnail" src="http://img.youtube.com/vi/4axPibsp4Ao/0.jpg" alt="video" /></a>
 			</div>
@@ -129,7 +129,7 @@
 					<p><strong>Views:<br></strong><em><span class="collectionDate">1260</span></em></p>
 				</div>
 			</div>
-			<p class="photoDesc text-left">
+			<p class="videoDesc text-left">
 			Bacon ipsum dolor sit amet cow swine chicken tri-tip t-bone, jowl short loin short ribs bresaola fra...
 			<button class="btn btn-primary btn-xs">More<span class="glyphicon glyphicon-chevron-right pull-right"></span></button>
 			</p>
@@ -138,10 +138,10 @@
 	<div class="row">
 		<div class="col-md-3 text-center panel panel-default">
 			<div class="row">
-				<h3 class="photoName">Youtube Video</h3>
+				<h3 class="videoName">Youtube Video</h3>
 				<div class="row">
-					<a class="editPhoto" title="Edit Photo" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
-					<a class="deletePhoto" title="Delete Photo" href="#"><span class="glyphicon glyphicon-trash"></span></a>
+					<a class="editvideo" title="Edit video" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
+					<a class="deletevideo" title="Delete video" href="#"><span class="glyphicon glyphicon-trash"></span></a>
 				</div>
 				<a class="fancybox-media" rel="group" href="http://www.youtube.com/watch?v=DLpUev1FvS0" ><img class="img-thumbnail" src="http://img.youtube.com/vi/DLpUev1FvS0/0.jpg" alt="video" /></a>
 			</div>
@@ -153,17 +153,17 @@
 					<p><strong>Views:<br></strong><em><span class="collectionDate">1260</span></em></p>
 				</div>
 			</div>
-			<p class="photoDesc text-left">
+			<p class="videoDesc text-left">
 			Bacon ipsum dolor sit amet cow swine chicken tri-tip t-bone, jowl short loin short ribs bresaola fra...
 			<button class="btn btn-primary btn-xs">More<span class="glyphicon glyphicon-chevron-right pull-right"></span></button>
 			</p>
 		</div>
 		<div class="col-md-3 text-center panel panel-default">
 			<div class="row">
-				<h3 class="photoName">Youtube Video</h3>
+				<h3 class="videoName">Youtube Video</h3>
 				<div class="row">
-					<a class="editPhoto" title="Edit Photo" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
-					<a class="deletePhoto" title="Delete Photo" href="#"><span class="glyphicon glyphicon-trash"></span></a>
+					<a class="editvideo" title="Edit video" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
+					<a class="deletevideo" title="Delete video" href="#"><span class="glyphicon glyphicon-trash"></span></a>
 				</div>
 				<a class="fancybox-media" rel="group" href="http://www.youtube.com/watch?v=0_W7fMSBeJg" ><img class="img-thumbnail" src="http://img.youtube.com/vi/0_W7fMSBeJg/0.jpg" alt="video" /></a>
 			</div>
@@ -175,17 +175,17 @@
 					<p><strong>Views:<br></strong><em><span class="collectionDate">1260</span></em></p>
 				</div>
 			</div>
-			<p class="photoDesc text-left">
+			<p class="videoDesc text-left">
 			Bacon ipsum dolor sit amet cow swine chicken tri-tip t-bone, jowl short loin short ribs bresaola fra...
 			<button class="btn btn-primary btn-xs">More<span class="glyphicon glyphicon-chevron-right pull-right"></span></button>
 			</p>
 		</div>
 		<div class="col-md-3 text-center panel panel-default">
 			<div class="row">
-				<h3 class="photoName">Youtube Video</h3>
+				<h3 class="videoName">Youtube Video</h3>
 				<div class="row">
-					<a class="editPhoto" title="Edit Photo" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
-					<a class="deletePhoto" title="Delete Photo" href="#"><span class="glyphicon glyphicon-trash"></span></a>
+					<a class="editvideo" title="Edit video" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
+					<a class="deletevideo" title="Delete video" href="#"><span class="glyphicon glyphicon-trash"></span></a>
 				</div>
 				<a class="fancybox-media" rel="group" href="http://www.youtube.com/watch?v=uF3fyQEB0f4" ><img class="img-thumbnail" src="http://img.youtube.com/vi/uF3fyQEB0f4/0.jpg" alt="video" /></a>
 			</div>
@@ -197,17 +197,17 @@
 					<p><strong>Views:<br></strong><em><span class="collectionDate">1260</span></em></p>
 				</div>
 			</div>
-			<p class="photoDesc text-left">
+			<p class="videoDesc text-left">
 			Bacon ipsum dolor sit amet cow swine chicken tri-tip t-bone, jowl short loin short ribs bresaola fra...
 			<button class="btn btn-primary btn-xs">More<span class="glyphicon glyphicon-chevron-right pull-right"></span></button>
 			</p>
 		</div>
 		<div class="col-md-3 text-center panel panel-default">
 			<div class="row">
-				<h3 class="photoName">Youtube Video</h3>
+				<h3 class="videoName">Youtube Video</h3>
 				<div class="row">
-					<a class="editPhoto" title="Edit Photo" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
-					<a class="deletePhoto" title="Delete Photo" href="#"><span class="glyphicon glyphicon-trash"></span></a>
+					<a class="editvideo" title="Edit video" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
+					<a class="deletevideo" title="Delete video" href="#"><span class="glyphicon glyphicon-trash"></span></a>
 				</div>
 				<a class="fancybox-media" rel="group" href="http://www.youtube.com/watch?v=92i_JqB6ffU" ><img class="img-thumbnail" src="http://img.youtube.com/vi/92i_JqB6ffU/0.jpg" alt="video" /></a>
 			</div>
@@ -219,7 +219,7 @@
 					<p><strong>Views:<br></strong><em><span class="collectionDate">1260</span></em></p>
 				</div>
 			</div>
-			<p class="photoDesc text-left">
+			<p class="videoDesc text-left">
 			Bacon ipsum dolor sit amet cow swine chicken tri-tip t-bone, jowl short loin short ribs bresaola fra...
 			<button class="btn btn-primary btn-xs">More<span class="glyphicon glyphicon-chevron-right pull-right"></span></button>
 			</p>
@@ -228,10 +228,10 @@
 	<div class="row">
 		<div class="col-md-3 text-center panel panel-default">
 			<div class="row">
-				<h3 class="photoName">Youtube Video</h3>
+				<h3 class="videoName">Youtube Video</h3>
 				<div class="row">
-					<a class="editPhoto" title="Edit Photo" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
-					<a class="deletePhoto" title="Delete Photo" href="#"><span class="glyphicon glyphicon-trash"></span></a>
+					<a class="editvideo" title="Edit video" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
+					<a class="deletevideo" title="Delete video" href="#"><span class="glyphicon glyphicon-trash"></span></a>
 				</div>
 				<a class="fancybox-media" rel="group" href="http://www.youtube.com/watch?v=YyqUfrmbrlA" ><img class="img-thumbnail" src="http://img.youtube.com/vi/YyqUfrmbrlA/0.jpg" alt="video" /></a>
 			</div>
@@ -243,17 +243,17 @@
 					<p><strong>Views:<br></strong><em><span class="collectionDate">1260</span></em></p>
 				</div>
 			</div>
-			<p class="photoDesc text-left">
+			<p class="videoDesc text-left">
 			Bacon ipsum dolor sit amet cow swine chicken tri-tip t-bone, jowl short loin short ribs bresaola fra...
 			<button class="btn btn-primary btn-xs">More<span class="glyphicon glyphicon-chevron-right pull-right"></span></button>
 			</p>
 		</div>
 		<div class="col-md-3 text-center panel panel-default">
 			<div class="row">
-				<h3 class="photoName">Youtube Video</h3>
+				<h3 class="videoName">Youtube Video</h3>
 				<div class="row">
-					<a class="editPhoto" title="Edit Photo" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
-					<a class="deletePhoto" title="Delete Photo" href="#"><span class="glyphicon glyphicon-trash"></span></a>
+					<a class="editvideo" title="Edit video" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
+					<a class="deletevideo" title="Delete video" href="#"><span class="glyphicon glyphicon-trash"></span></a>
 				</div>
 				<a class="fancybox-media" rel="group" href="http://www.youtube.com/watch?v=aE2GCa-_nyU" ><img class="img-thumbnail" src="http://img.youtube.com/vi/aE2GCa-_nyU/0.jpg" alt="video" /></a>
 			</div>
@@ -265,17 +265,17 @@
 					<p><strong>Views:<br></strong><em><span class="collectionDate">1260</span></em></p>
 				</div>
 			</div>
-			<p class="photoDesc text-left">
+			<p class="videoDesc text-left">
 			Bacon ipsum dolor sit amet cow swine chicken tri-tip t-bone, jowl short loin short ribs bresaola fra...
 			<button class="btn btn-primary btn-xs">More<span class="glyphicon glyphicon-chevron-right pull-right"></span></button>
 			</p>
 		</div>
 		<div class="col-md-3 text-center panel panel-default">
 			<div class="row">
-				<h3 class="photoName">Youtube Video</h3>
+				<h3 class="videoName">Youtube Video</h3>
 				<div class="row">
-					<a class="editPhoto" title="Edit Photo" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
-					<a class="deletePhoto" title="Delete Photo" href="#"><span class="glyphicon glyphicon-trash"></span></a>
+					<a class="editvideo" title="Edit video" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
+					<a class="deletevideo" title="Delete video" href="#"><span class="glyphicon glyphicon-trash"></span></a>
 				</div>
 				<a class="fancybox-media" rel="group" href="http://www.youtube.com/watch?v=sf6LD2B_kDQ" ><img class="img-thumbnail" src="http://img.youtube.com/vi/sf6LD2B_kDQ/0.jpg" alt="video" /></a>
 			</div>
@@ -287,17 +287,17 @@
 					<p><strong>Views:<br></strong><em><span class="collectionDate">1260</span></em></p>
 				</div>
 			</div>
-			<p class="photoDesc text-left">
+			<p class="videoDesc text-left">
 			Bacon ipsum dolor sit amet cow swine chicken tri-tip t-bone, jowl short loin short ribs bresaola fra...
 			<button class="btn btn-primary btn-xs">More<span class="glyphicon glyphicon-chevron-right pull-right"></span></button>
 			</p>
 		</div>
 		<div class="col-md-3 text-center panel panel-default">
 			<div class="row">
-				<h3 class="photoName">Youtube Video</h3>
+				<h3 class="videoName">Youtube Video</h3>
 				<div class="row">
-					<a class="editPhoto" title="Edit Photo" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
-					<a class="deletePhoto" title="Delete Photo" href="#"><span class="glyphicon glyphicon-trash"></span></a>
+					<a class="editvideo" title="Edit video" href="#"><span class="glyphicon glyphicon-edit"></span></a>&nbsp&nbsp&nbsp
+					<a class="deletevideo" title="Delete video" href="#"><span class="glyphicon glyphicon-trash"></span></a>
 				</div>
 				<a class="fancybox-media" rel="group" href="http://www.youtube.com/watch?v=Pa9qEX5KwUc" ><img class="img-thumbnail" src="http://img.youtube.com/vi/Pa9qEX5KwUc/0.jpg" alt="video" /></a>
 			</div>
@@ -309,7 +309,7 @@
 					<p><strong>Views:<br></strong><em><span class="collectionDate">1260</span></em></p>
 				</div>
 			</div>
-			<p class="photoDesc text-left">
+			<p class="videoDesc text-left">
 			Bacon ipsum dolor sit amet cow swine chicken tri-tip t-bone, jowl short loin short ribs bresaola fra...
 			<button class="btn btn-primary btn-xs">More<span class="glyphicon glyphicon-chevron-right pull-right"></span></button>
 			</p>
