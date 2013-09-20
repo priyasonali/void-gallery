2944424815309882256f0f6ee8dc42969d8ff448
santakmishra <1337shan@gmail.com>
2013-09-20 21:03:56 +0530

    Minor Fix to Photo upload

--- source/gallery/uploadhandler.php
+++ source/gallery/uploadhandler.php
@@ -10,12 +10,12 @@ while(file_exists('uploads/photos/' . $fname)){
 $fname=substr(md5(uniqid(rand(), true)), 16, 10).".jpg";
 }
 
-if($_FILES['item']['error'] > 0) die("Unable to handle upload. Corrupt file or file too large.");
-else if($_FILES['item']['size'] > 1000000) die("Maximum allowed file size of 1MB exceeded.");
-else if($extension != $allowedExt) die("Invalid extension. Only .jpg allowed.");
-else if(!getimagesize($_FILES['item']['tmp_name'])) die("Invalid file format or not an image.");
-else if(!($_FILES['item']['type'] == 'image/jpg' || $_FILES['item']['type'] == 'image/jpeg')) die("Invalid file type. Not JPEG or corrupt file.");
-else if(!move_uploaded_file($_FILES['item']['tmp_name'], 'uploads/photos/' . $fname)) die("Unable to upload. Denied access at server.");
+if($_FILES['item']['error'] > 0) echo "<p class='text-danger'>Unable to handle upload. Corrupt file or file too large. <a href=''>Click here</a> to retry.</p>";
+else if($_FILES['item']['size'] > 1000000) echo "<p class='text-danger'>Maximum allowed file size of 1MB exceeded. <a href=''>Click here</a> to retry.</p>";
+else if($extension != $allowedExt) echo "<p class='text-danger'>Invalid extension. Only .jpg allowed. <a href=''>Click here</a> to retry.</p>";
+else if(!getimagesize($_FILES['item']['tmp_name'])) echo "<p class='text-danger'>Invalid file format or not an image. <a href=''>Click here</a> to retry.</p>";
+else if(!($_FILES['item']['type'] == 'image/jpg' || $_FILES['item']['type'] == 'image/jpeg')) echo "<p class='text-danger'>Invalid file type. Not JPEG or corrupt file. <a href=''>Click here</a> to retry.</p>";
+else if(!move_uploaded_file($_FILES['item']['tmp_name'], 'uploads/photos/' . $fname)) echo "<p class='text-danger'>Unable to upload. Denied access at server. <a href=''>Click here</a> to retry.</p>";
 
 else{
 echo '
