dccb40a4397512e9a8c71d8dda66c8a7cd32106e
Santak Kumar Mishra <1337shan@gmail.com>
2013-09-23 16:58:27 +0530

    Added latest SQL dump

--- source/gallery/sqldump/gallery.sql
+++ source/gallery/sqldump/gallery.sql
@@ -1,32 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 3.5.2.2
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Sep 16, 2013 at 07:32 PM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Database: `gallery`
---
 CREATE DATABASE `gallery` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `gallery`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `assign`
---
 
 CREATE TABLE IF NOT EXISTS `assign` (
   `aid` int(5) NOT NULL AUTO_INCREMENT,
@@ -35,12 +16,6 @@ CREATE TABLE IF NOT EXISTS `assign` (
   `vid` int(5) NOT NULL,
   PRIMARY KEY (`aid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `collection`
---
 
 CREATE TABLE IF NOT EXISTS `collection` (
   `cid` int(5) NOT NULL AUTO_INCREMENT,
@@ -51,12 +26,6 @@ CREATE TABLE IF NOT EXISTS `collection` (
   PRIMARY KEY (`cid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `photo`
---
-
 CREATE TABLE IF NOT EXISTS `photo` (
   `pid` int(5) NOT NULL AUTO_INCREMENT,
   `pname` varchar(20) NOT NULL,
@@ -64,14 +33,9 @@ CREATE TABLE IF NOT EXISTS `photo` (
   `pdate` varchar(20) NOT NULL,
   `pstatus` int(1) NOT NULL DEFAULT '1',
   `pview` int(5) NOT NULL,
+  `pcode` varchar(100) NOT NULL,
   PRIMARY KEY (`pid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `video`
---
 
 CREATE TABLE IF NOT EXISTS `video` (
   `vid` int(11) NOT NULL AUTO_INCREMENT,
@@ -80,8 +44,9 @@ CREATE TABLE IF NOT EXISTS `video` (
   `vdate` varchar(20) NOT NULL,
   `vstatus` int(1) NOT NULL DEFAULT '1',
   `vview` int(5) NOT NULL,
+  `vcode` varchar(100) NOT NULL,
   PRIMARY KEY (`vid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
