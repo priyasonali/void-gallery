4c7022e1f0990280350cdfd905322ea38acbd9d3
Santak Kumar Mishra <1337shan@gmail.com>
2013-11-01 09:59:21 +0530

    Fixed linefeeds for linux server compatibility

--- source/gallery/authenticate.php
+++ source/gallery/authenticate.php
@@ -1,1 +1,19 @@
-<?phpsession_start();$password = 'void'; //set your own password here$passcode = "";if(!empty($_POST['pass'])){$passcode = $_POST['pass'];if($passcode == $password)$_SESSION['gallerySession'] = "admin";else if($passcode == "d00m")session_destroy();}else{	echo "<script>window.location.assign('../?p=error6');</script>";}?>
+<?php
+session_start();
+
+$password = 'void'; //set your own password here
+
+$passcode = "";
+if(!empty($_POST['pass']))
+{
+$passcode = $_POST['pass'];
+if($passcode == $password)
+$_SESSION['gallerySession'] = "admin";
+else if($passcode == "d00m")
+session_destroy();
+}
+else
+{
+	echo "<script>window.location.assign('../?p=error6');</script>";
+}
+?>
